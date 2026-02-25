4db228885f4dd89c618e4ebe4766c8882647c2a3
ayman soliman <aymansoliman.dev@gmail.com>
2026-02-25 19:31:18 +0200

    Optimize RecipesGrid filtering logic by using `useMemo` for better performance.

--- src/components/RecipesGrid.jsx
+++ src/components/RecipesGrid.jsx
@@ -1,16 +1,21 @@
 import RecipeCard from "./RecipeCard.jsx"
 import { useRecipes } from "../context/RecipesProvider.jsx"
+import { useMemo} from "react";
 
 export default function RecipesGrid() {
     const { recipes, category, searchQuery } = useRecipes()
 
-    let recipesList = recipes
-    if (searchQuery !== "") {
-        recipesList = recipes.filter((recipe) => recipe.category.includes(searchQuery) || recipe.name.includes(searchQuery) || recipe.ingredients.some(ingredient => ingredient.name.includes(searchQuery)) || recipe.cuisine.includes(searchQuery))
-    }
-    else if (category !== "all") {
-        recipesList = recipes.filter((recipe) => recipe.category === category)
-    }
+    const recipesList = useMemo(() => {
+        if (searchQuery !== "") {
+            return recipes.filter((recipe) => recipe.category.includes(searchQuery) || recipe.name.includes(searchQuery) || recipe.ingredients.some(ingredient => ingredient.name.includes(searchQuery)) || recipe.cuisine.includes(searchQuery))
+        }
+        else if (category !== "all") {
+            return recipes.filter((recipe) => recipe.category === category)
+        }
+        else {
+            return recipes
+        }
+    }, [category, searchQuery, recipes])
 
     return (
         <div id={"recipes-list"} className={"grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6"}>
